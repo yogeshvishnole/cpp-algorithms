8dc8793fecc91f0fa3e6668a4964ae0ca64a2272
Yogesh vishnole <yogeshvishnole@gmail.com>
2020-03-28 12:54:06 +0530

    Binary tree class added with several functions

--- Implementation of  tree datastructure using linkr=ed list/Queue.h
+++ Implementation of  tree datastructure using linkr=ed list/Queue.h
@@ -1,13 +1,17 @@
-/*
- Implementation of queue data structure using the concept of arrays
-Author : Yogesh vishnole
-
-*/
-
 #include<iostream>
 using namespace std;
 
+#ifndef QUEUE_H_INCLUDED
+#define QUEUE_H_INCLUDED
 
+class Node
+{
+public:
+    Node *lchild;
+    int data;
+    Node *rchild;
+
+};
 
 class Queue
 {
@@ -15,24 +19,24 @@ private:
     int size;
     int front;
     int rear;
-    int *q;
+    Node **q;
 public:
     Queue()
     {
         size = 10;
         front = -1;
         rear = -1;
-        q = new int[10];
+        q = new Node*[10];
     }
     Queue(int size)
     {
         this->size = size;
         front = -1;
         rear = -1;
-        q = new int[size];
+        q = new Node*[size];
     }
 
-    void enqueue(int item)
+    void enqueue(Node *item)
     {
       if(rear == size-1)
       {
@@ -46,9 +50,9 @@ public:
 
     }
 
-    int dequeue()
+    Node *dequeue()
     {
-        int x;
+        Node *x = NULL;
         if(front == rear)
         {
             cout<<"Queue is empty"<<endl;
@@ -69,16 +73,7 @@ public:
 
 };
 
-int main()
-{
-    Queue *q = new Queue[10];
-    q->enqueue(10);
-    q->enqueue(20);
-    q->enqueue(30);
 
-    cout<<q->isEmpty()<<endl;
-    cout<<q->dequeue()<<endl;
-    cout<<q->dequeue()<<endl;
-    cout<<q->dequeue()<<endl;
-    cout<<q->isEmpty()<<endl;
-}
+
+
+#endif // QUEUE_H_INCLUDED
---
+++ Implementation of  tree datastructure using linkr=ed list/Stack.h
@@ -0,0 +1,57 @@
+#include<iostream>
+#include "Queue.h"
+using namespace std;
+
+#ifndef STACK_H_INCLUDED
+#define STACK_H_INCLUDED
+
+// Here  we implement the stack datastructure using the linked list
+
+class StackNode
+{
+public:
+    Node *data;
+    StackNode *next;
+};
+
+class Stack
+{
+    StackNode *top;
+public:
+
+    Stack()
+    {
+       top = NULL;
+    }
+
+    void push(Node *data)
+    {
+        StackNode *temp;
+        temp = new StackNode;
+        temp->data = data;
+        temp->next = top;
+        top = temp;
+    }
+
+    Node *pop()
+    {
+        Node *x = top->data;
+        StackNode *t = top;
+        top = top->next;
+        delete t;
+        return x;
+    }
+
+    bool isEmpty()
+    {
+        if(top == NULL)
+            return true;
+        return false;
+    }
+
+};
+
+
+
+#endif // STACK_H_INCLUDED
+
